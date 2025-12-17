f8e2ac1f26b6244842036d9caa9daf3c0ada6d9a
juankalebe <ojuankalebe@gmail.com>
2025-12-17 12:15:31 -0300

    Exercicios 3 e 3D

---
+++ exercicios/exercicio-3D/funcoes-criadas.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "funcoes-criadas.h"
+
+int somatorio(int vetor[],int tam){
+    int soma=0;
+    for(int i=0;i<tam;i++)
+        soma = soma + vetor[i];
+    return soma;
+}
+
+float media_aritmetica(float vetor[],int tam){
+    float soma=0;
+
+    for(int i=0;i<tam;i++)
+        soma = soma + vetor[i];
+    return (soma/tam);
+}
+
+void limpar_buffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
