4e77b3ff76f9b7e141c7b1d6fc47c3a17827a475
juankalebe <ojuankalebe@gmail.com>
2025-12-18 07:51:35 -0300

    Exercicio 5

---
+++ exercicios/exercicio-5/exercicio-5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*17.12.25 Faça um programa que receba dois números e mostre o maior deles. 
+Caso eles sejam iguais, deve-se mostrar a mensagem "Os números são iguais".*/
+
+int main (){
+    setlocale(LC_ALL, "Portuguese");
+
+    float num2, num1;
+
+    printf("Insira o um número: ");
+    scanf("%f",&num1);
+
+    printf("Insira o outro número: ");
+    scanf("%f",&num2);
+
+    if (num1 > num2){
+    printf("==========================");
+    printf("\n%.2f é maior do que %.2f\n",num1,num2);
+    printf("==========================\n");       
+    } else if(num1 < num2){
+    printf("==========================");
+    printf("\n%.2f é maior do que %.2f\n",num2,num1);
+    printf("==========================\n");         
+    } else{
+    printf("==========================");
+    printf("\nOs números são iguais\n");
+    printf("==========================\n");          
+    }
+
+    return 0;
+}
