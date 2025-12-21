eb1b691069dc00ff71f6252f9124dd2195de8cbc
juankalebe <ojuankalebe@gmail.com>
2025-12-21 00:45:47 -0300

    Exercicio 10

---
+++ exercicios/exercicio-10/exercicio-10.c
@@ -0,0 +1,21 @@
+/*21.12.25 Desenvolva um algoritmo que mostre a tabuada de todos os números inteiros compreendidos entre 1 e 10 (inclusive)*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main (){
+    setlocale(LC_ALL, "Portuguese");
+       
+    int i, j;
+
+    for (i = 1; i <= 10; i++){
+        printf("Tabuada do %d:\n",i);
+        for (j = 1; j <= 10; j++){
+            printf("%d x %d = %d\n",i,j,i*j);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
